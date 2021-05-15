4d4c986de0d07da4bc9dd5636a24ca992bb7cfd1
acuon <83219534+acuon@users.noreply.github.com>
2021-05-15 16:52:02 +0530

    Update ShuffletheArray.java

--- Array/ShuffletheArray.java
+++ Array/ShuffletheArray.java
@@ -49,3 +49,21 @@ class Solution {
     return output;  
     }
 }
+
+/*
+mausam's code
+
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+      int length=nums.length;
+        int[] new_nums=new int[length];
+        for(int i=0; i<n; i++){
+            new_nums[2*i] = nums[i];
+            new_nums[2*i+1]=nums[n+i];
+            
+        }
+        return new_nums;
+    }
+}
+*/
