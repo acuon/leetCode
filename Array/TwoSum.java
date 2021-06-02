401b6b0e80f23500122dbd7609b1f06edbdf0aaf
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-02 18:34:24 +0530

    Using HashMap and Arrays

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -37,6 +37,35 @@ Constraints:
 
 */
 
+
+//Using HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.containsKey(target - nums[i])) {
+//                 return new int[] { i, map.get(target - nums[i]) };
+//             }
+
+//             map.put(nums[i], i);
+//         }
+
+//         return new int[0];
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[] {i, map.get(target-nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[0];
+    }
+}
+
+
+
+//Using Arrays
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] output = new int[2];
@@ -57,3 +86,6 @@ class Solution {
     return output;    
     }
 }
+
+
+
