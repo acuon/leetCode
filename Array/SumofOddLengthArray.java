155d6e45362b28155c18de7dd36cc48c415ebae5
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-02 18:49:41 +0530

    Brute force and optimized

--- Array/SumofOddLengthArray.java
+++ Array/SumofOddLengthArray.java
@@ -1,4 +1,6 @@
 /*
+//https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+
 Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
 
 A subarray is a contiguous subsequence of the array.
@@ -54,6 +56,8 @@ class Solution {
 
 
 /*
+//Brute Force
+
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         int sum = 0;
