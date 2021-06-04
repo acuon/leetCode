15113397ef7ab35d802ace1d14334cdd7bd32ecd
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-04 19:23:56 +0530

    Update CountItemsMatchingaRule.java

--- Array/CountItemsMatchingaRule.java
+++ Array/CountItemsMatchingaRule.java
@@ -57,7 +57,27 @@ class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int count = 0;
         int index = 0;
-        if(ruleKey.equals("type")) {
+        if (ruleKey.equals("color")) {
+            index = 1;
+        } else if (ruleKey.equals("name")) {
+            index = 2;
+        }
+        for(int i=0; i<items.size(); i++) {
+            if(items.get(i).get(index).equals(ruleValue)) {
+                count++;
+            }  
+        }
+        return count;
+    }
+}
+*/
+
+/*
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        int index = 0;
+        if(ruleKey.equals("type")) {             // not necessary since index is already initialized at 0
             index = 0;
         } else if (ruleKey.equals("color")) {
             index = 1;
