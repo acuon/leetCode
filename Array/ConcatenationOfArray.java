d198278ceb9a455f5cfe0b6184e7a9a64eeb6ccf
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-07-31 22:16:19 +0530

    Create ConcatenationOfArray.java

---
+++ Array/ConcatenationOfArray.java
@@ -0,0 +1,75 @@
+/*
+//https://leetcode.com/problems/concatenation-of-array/
+
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+
+Example 2:
+
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+
+ 
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 1000
+    1 <= nums[i] <= 1000
+
+*/
+
+//2ms Runtime
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length*2];
+        for(int i=0; i<res.length; i++) {
+            res[i] = nums[i%nums.length];
+        }
+        return res;
+    }
+}
+
+
+
+//1ms Runtime
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length*2];
+        for(int i=0; i<nums.length; i++) {
+            res[i] = res[i+nums.length] = nums[i%nums.length];
+        }
+        return res;
+    }
+}
+
+
+
+//0ms Runtime
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] arr = new int[2*nums.length];
+        for(int i=0;i<nums.length;i++) {
+            arr[i]=nums[i];
+            arr[i+nums.length]=nums[i];
+        }
+        return arr;
+    }
+}
+
