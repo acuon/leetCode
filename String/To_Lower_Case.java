057e04f6eb46b4f101a1c5fd343acad23b7e101e
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-08-16 17:02:23 +0530

    Update To_Lower_Case.java

--- String/To_Lower_Case.java
+++ String/To_Lower_Case.java
@@ -45,3 +45,25 @@ class Solution {
         return new String(ar);
     }
 }
+
+//using BitWise OR
+class Solution {
+    public String toLowerCase(String s) {
+        char[] ar = s.toCharArray();
+        for(int i=0; i<ar.length; i++) {
+            if(ar[i]>=65 && ar[i]<=90) {
+                ar[i] = (char)(ar[i]|(char)32);
+            }
+        }
+        return new String(ar);
+    }
+}
+
+//using InputStream
+class Solution {
+    public String toLowerCase(String s) {
+        char[] a = s.toCharArray();
+        IntStream.range(0, a.length).filter(i -> 'A' <= a[i] && a[i] <= 'Z').forEach(i -> a[i] = (char) (a[i] - 'A' + 'a'));
+        return new String(a);
+    }
+}
