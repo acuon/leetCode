277465320b447cbacd5c8abd5ffc9974a8526344
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-08-26 17:38:21 +0530

    Update BuildArrayFromPermutation.java

--- Array/BuildArrayFromPermutation.java
+++ Array/BuildArrayFromPermutation.java
@@ -25,7 +25,6 @@ ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]]
     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
     = [4,5,0,1,2,3]
 
- 
 
 Constraints:
 
@@ -34,6 +33,7 @@ Constraints:
     The elements in nums are distinct.
     
 */
+
 //1ms Runtime
 class Solution {
     public int[] buildArray(int[] nums) {
