8d8b1fa353a3f2cbcaa0477dba32dde78a63ec32
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-09-17 15:21:32 +0530

    Update Remove_Duplicates _from_Sorted_List.java

--- Linked List/Remove_Duplicates _from_Sorted_List.java
+++ Linked List/Remove_Duplicates _from_Sorted_List.java
@@ -47,3 +47,21 @@ class Solution {
         return head;
     }
 }
+
+
+//1ms Runtime
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        
+        while(temp!=null && temp.next!=null) {
+            if(temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        
+        return head;
+    }
+}
