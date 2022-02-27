954898427e8f762ed9e15092cbe042fa4fff9df3
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-02-27 08:45:57 +0530

    Create MinimumNumberofStepstoMakeTwoStringsAnagramII.java

---
+++ Weekly/27-02-2022/6009 Minimum Number of Steps to Make Two Strings Anagram II/MinimumNumberofStepstoMakeTwoStringsAnagramII.java
@@ -0,0 +1,18 @@
+//
+
+class Solution {
+    public int minSteps(String s, String t) {
+        int[] arr = new int[26];
+        int count = 0;
+        for(int i=0; i<s.length(); i++) {
+            arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<t.length(); i++) {
+            arr[t.charAt(i)-'a']--;
+        }
+        for(int i: arr) {
+            count += Math.abs(i);
+        }
+        return count;
+    }
+}
