21a0fa5057c53525a2a6906a64b84223e2f334af
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-03-23 19:59:17 +0530

    Create BrokenCalculator.java

---
+++ Problems/0991 Broken Calculator/BrokenCalculator.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/broken-calculator/
+
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        if(startValue >= target) return startValue-target;
+        if(target%2==0) {
+            return 1 + brokenCalc(startValue, target/2);
+        } else {
+            return 1 + brokenCalc(startValue, target+1);
+        }
+    }
+}
