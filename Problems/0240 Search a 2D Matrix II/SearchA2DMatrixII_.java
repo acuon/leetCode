bd5969d5250fe97614dc55cdf55156521ba986ca
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-03-31 17:44:17 +0530

    Create SearchA2DMatrixII_.java

---
+++ Problems/0240 Search a 2D Matrix II/SearchA2DMatrixII_.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n-1;
+        while(row<m && col>=0) {
+            if(matrix[row][col] == target) {
+                return true;
+            } else if(matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
