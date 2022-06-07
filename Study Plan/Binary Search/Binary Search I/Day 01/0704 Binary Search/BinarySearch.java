36fe76c6288fcb509c0db06c114aa765a2263548
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-06-07 15:20:44 +0530

    Create BinarySearch.java

---
+++ Study Plan/Binary Search/Binary Search I/Day 01/0704 Binary Search/BinarySearch.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/binary-search/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j) {
+            int mid = (i+j)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid]<target) {
+                i=mid+1;
+            } else {
+                j=mid-1;
+            }
+        }
+        return -1;
+    }
+}
