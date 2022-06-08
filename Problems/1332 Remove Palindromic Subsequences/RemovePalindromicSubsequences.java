c88216df025aa37fc2b572131a80e3efc234afd2
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-06-08 13:00:56 +0530

    Update RemovePalindromicSubsequences.java

--- Problems/1332 Remove Palindromic Subsequences/RemovePalindromicSubsequences.java
+++ Problems/1332 Remove Palindromic Subsequences/RemovePalindromicSubsequences.java
@@ -1,6 +1,21 @@
 //https://leetcode.com/problems/remove-palindromic-subsequences/
 //https://leetcode.com/problems/remove-palindromic-subsequences/discuss/2125596/Easy-Solution-with-Explanation
 
+/**
+Note :
+
+    Before writing the code, let's first look at the hints given in the question:
+        the string consists of only letters a and b.
+        we have to remove palindromic subsequence not substring.
+
+Explanation of Code :
+
+    after looking at the above hints, we can conclude there are only 3 cases:-
+        String length is 0 -> If the string length is 0, we can simply return 0.
+        Entire string is a palindrome -> If the entire string is a palindrome, we can remove the whole string in single move. so in this case, we return 1.
+        Entire string is not a palindrome -> If the given string is not an palindrome. Then first look at the first hint given above, the string only contains a's and b's. we can first remove all the a letter from the string in one move and then all the b letter in second move, and vice versa.
+*/
+
 class Solution {
     public int removePalindromeSub(String s) {
         if(s.length() == 0) return 0;
@@ -12,7 +27,3 @@ class Solution {
         return 1;
     }
 }
-//it says subsequence not substring
-//lets suppose the string is aaaba
-// then we can remove aaaa subsequence in first move
-// and b in second move
