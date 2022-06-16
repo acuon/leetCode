d0a8e33974386953380a82af03d888b14c1495ee
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-06-16 22:19:59 +0530

    Create ArrangingCoins.java

---
+++ Study Plan/Binary Search/Binary Search I/Day 06/0441 Arranging Coins/ArrangingCoins.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/arranging-coins/
+
+class Solution {
+    public int arrangeCoins(int n) {
+        int i = 1;
+        while(n>0) {
+            i++;
+            n-=i;
+        }
+        return i-1;
+    }
+}
