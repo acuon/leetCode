0d94d38bf40335d7e1035a2e172345f54bd46047
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-06-24 12:48:58 +0530

    Create ConstructTargetArrayWithMultipleSums.java

---
+++ Problems/1354 Construct Target Array With Multiple Sums/ConstructTargetArrayWithMultipleSums.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/construct-target-array-with-multiple-sums/
+
+class Solution {
+    public boolean isPossible(int[] target) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> (b - a));
+        long total = 0;
+        for (int a : target) {
+            total += a;
+            pq.add(a);
+        }
+        while (true) {
+            int a = pq.poll();
+            total -= a;
+            if (a == 1 || total == 1)
+                return true;
+            if (a < total || total == 0 || a % total == 0)
+                return false;
+            a %= total;
+            total += a;
+            pq.add(a);
+        }
+    }
+}
