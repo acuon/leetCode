923679464b8805a7f485ccddea04bed8e8dbe6af
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-06-30 10:54:15 +0530

    Create MinimumMovesToEqualArrayElementsII.java

---
+++ Problems/0462 Minimum Moves to Equal Array Elements II/MinimumMovesToEqualArrayElementsII.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length-1;
+        int count = 0;
+        while(i < j){
+            count += nums[j]-nums[i];
+            i++;
+            j--;
+        }
+        return count;
+    }
+}
