9a81f0c7c86e5f9c2aee7756982518ed2e6f5724
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-07-15 20:51:49 +0530

    Update MaxAreaOfIsland.java

--- Problems/0695 Max Area of Island/MaxAreaOfIsland.java
+++ Problems/0695 Max Area of Island/MaxAreaOfIsland.java
@@ -2,33 +2,29 @@
 
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        if (grid == null || grid.length == 0) {
+        if(grid == null || grid.length == 0) {
             return 0;
         }
-        int m = grid.length;
-        int n = grid[0].length;
-        int max = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int area = dfs(grid, i, j, m, n, 0);
-                    max = Math.max(area, max);
+        int area = 0;
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[i].length; j++) {
+                if(grid[i][j] == 1) {
+                    area = Math.max(area, dfs(grid, i, j, 0));
                 }
             }
         }
-        return max;
+        return area;
     }
-
-    public int dfs(int[][] grid, int i, int j, int m, int n, int area) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
-            return area;
+    public int dfs(int[][] grid, int i, int j, int area) {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0) {
+            return 0;
         }
         grid[i][j] = 0;
         area++;
-        area = dfs(grid, i + 1, j, m, n, area);
-        area = dfs(grid, i, j + 1, m, n, area);
-        area = dfs(grid, i - 1, j, m, n, area);
-        area = dfs(grid, i, j - 1, m, n, area);
+        area += dfs(grid, i-1, j, 0);
+        area += dfs(grid, i+1, j, 0);
+        area += dfs(grid, i, j-1, 0);
+        area += dfs(grid, i, j+1, 0);
         return area;
     }
 }
