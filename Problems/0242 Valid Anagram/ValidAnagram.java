179ebf3e9bd7c7e0fe77bf91f279e21f117a4489
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-07-28 08:47:54 +0530

    Create ValidAnagram.java

---
+++ Problems/0242 Valid Anagram/ValidAnagram.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[26];
+        for(int i=0; i<s.length(); i++) {
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        for(int i=0; i<26; i++) {
+            if(arr[i] != 0) return false;
+        }
+        return true;
+    }
+}
