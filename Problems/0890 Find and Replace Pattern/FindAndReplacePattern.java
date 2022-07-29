dfd2b8dd40ca35c658d731d0d1208bdfd2b23a53
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-07-29 22:29:59 +0530

    Create FindAndReplacePattern.java

---
+++ Problems/0890 Find and Replace Pattern/FindAndReplacePattern.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/find-and-replace-pattern/
+
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+        for (String word : words) {
+            if (check(word, pattern)) res.add(word);
+        }
+        return res;
+    }
+    
+    boolean check(String a, String b) {
+        for (int i = 0; i < a.length(); i++) {
+            if (a.indexOf(a.charAt(i)) != b.indexOf(b.charAt(i))) return false;
+        }
+        return true;
+    }
+}
