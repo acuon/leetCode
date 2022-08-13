36226937b1494fb45e376fd2138075564b855905
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-08-13 22:09:17 +0530

    Create SubstringWithConcatenationOfAllWords.java

---
+++ Problems/0030 Substring with Concatenation of All Words/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans = new ArrayList<Integer>();
+		int len = words[0].length(); // length of each word
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+		for (int i = 0; i <= s.length() - len * words.length; i++) {
+			Map<String, Integer> copyMap = new HashMap<String, Integer>(map);
+			for (int j = 0; j < words.length; j++) {
+				String str = s.substring(i + j*len, i + j*len + len);
+				if (copyMap.containsKey(str)) {
+					int count = copyMap.get(str);
+					if (count == 1) copyMap.remove(str);
+					else copyMap.put(str, count - 1);
+					if (copyMap.isEmpty()) {
+						ans.add(i);
+						break;
+					}
+				} else { 
+                    break;
+                }
+			}
+		}
+		return ans;
+    }
+}
