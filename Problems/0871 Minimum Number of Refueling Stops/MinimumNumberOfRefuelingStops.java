166ea53d72efd513d045fe56f2f4caacf16b1b28
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-08-20 20:29:05 +0530

    Create MinimumNumberOfRefuelingStops.java

---
+++ Problems/0871 Minimum Number of Refueling Stops/MinimumNumberOfRefuelingStops.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/minimum-number-of-refueling-stops/
+
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        PriorityQueue<Integer> fuelInStation = new PriorityQueue<>((x, y) -> (y - x));
+        
+        int res = 0;
+        int fuel = startFuel;
+        
+        for (int[] station : stations) {
+            while (fuel < station[0]) {
+                if (fuelInStation.isEmpty()) return -1;
+                fuel += fuelInStation.poll();
+                res++;
+            }
+            fuelInStation.offer(station[1]);
+        }
+        
+        while (fuel < target) {
+            if (fuelInStation.isEmpty()) return -1;
+            fuel += fuelInStation.poll();
+            res++;
+        }
+        
+
+        return res;
+    }
+}
