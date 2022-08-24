e31c9e17b5e742cd3ebec45469d6ff4844b7de19
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-08-24 22:43:26 +0530

    Create PowerOfThree.java

---
+++ Problems/0326 Power of Three/PowerOfThree.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/power-of-three/
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        while(n%3==0) {
+            n/=3;
+        }
+        return n == 1;
+    }
+}
