1bec769c99aa0c3a8b442b0b7b330e644976681a
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-08-25 10:18:21 +0530

    Create RansomNote.java

---
+++ Problems/0383 Ransom Note/RansomNote.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/ransom-note/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for(char ch: magazine.toCharArray()) {
+            arr[ch-'a']++;
+        }
+        for(char ch: ransomNote.toCharArray()) {
+            if(--arr[ch-'a'] < 0) return false;
+        }
+        return true;
+    }
+}
