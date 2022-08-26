2fc3cc5b25e0947b4d37e35ee31b64f9bf29ac02
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-08-26 13:36:36 +0530

    Update ReorderedPowerOf2.java

--- Problems/0869 Reordered Power of 2/ReorderedPowerOf2.java
+++ Problems/0869 Reordered Power of 2/ReorderedPowerOf2.java
@@ -1,1 +1,18 @@
-//
+//https://leetcode.com/problems/reordered-power-of-2/
+
+class Solution {
+    public boolean reorderedPowerOf2(int num) {
+        var numArr = String.valueOf(num).toCharArray();
+        Arrays.sort(numArr);
+        for(int i=0; i<=30; i++) {
+            int power = 1<<i;
+            if(isEqual(numArr, power)) return true;
+        }
+        return false;
+    }
+    public static boolean isEqual(char[] numArr, int power) {
+        var powerArr = String.valueOf(power).toCharArray();
+        Arrays.sort(powerArr);
+        return Arrays.equals(numArr, powerArr);
+    }
+}
