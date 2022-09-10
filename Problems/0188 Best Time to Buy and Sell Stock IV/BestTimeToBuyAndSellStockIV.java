c5a46dea2c13e510d7428e0c03eda97d6937de0e
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-09-10 18:55:44 +0530

    Create BestTimeToBuyAndSellStockIV.java

---
+++ Problems/0188 Best Time to Buy and Sell Stock IV/BestTimeToBuyAndSellStockIV.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        return DP(prices, k);
+    }
+    
+    private int DP(int[] prices, int k) {
+        int[] costs = new int[k+1];
+        int[] profits = new int[k+1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        
+        for (int currentPrice : prices) {
+            for (int i = 0; i < k; i++) {
+                costs[i+1] = Math.min(costs[i+1], currentPrice - profits[i]);
+                profits[i+1] = Math.max(profits[i+1], currentPrice - costs[i+1]);
+            }
+        }
+        
+        return profits[k];
+    }
+}
