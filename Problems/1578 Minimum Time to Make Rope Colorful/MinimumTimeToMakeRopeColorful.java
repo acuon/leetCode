917b3af49479dac2bf257c217013617894374e98
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-10-03 20:57:23 +0530

    Create MinimumTimeToMakeRopeColorful.java

---
+++ Problems/1578 Minimum Time to Make Rope Colorful/MinimumTimeToMakeRopeColorful.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int minCost = 0;
+        for(int i=1; i<colors.length(); i++) {
+            if(colors.charAt(i) == colors.charAt(i-1)) {
+                minCost += Math.min(neededTime[i], neededTime[i-1]);
+                neededTime[i] = Math.max(neededTime[i], neededTime[i-1]);
+            }
+        }
+        return minCost;
+    }
+}
