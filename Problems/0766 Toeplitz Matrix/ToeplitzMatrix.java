c316af50462f47f204ccc033f589821403827cb6
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-10-31 17:00:54 +0530

    Create ToeplitzMatrix.java

---
+++ Problems/0766 Toeplitz Matrix/ToeplitzMatrix.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/toeplitz-matrix/
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=0; i<matrix.length-1; i++) {
+            for(int j=0; j<matrix[0].length-1; j++) {
+                if(matrix[i][j] != matrix[i+1][j+1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
